c9953d78e1cbbd19adefc045e08f411c7793481d
PumaYT22 <pumaxyt@gmail.com>
2024-06-16 18:50:00 +0200

    Update App.js

--- Strona/egzaminy/src/App.js
+++ Strona/egzaminy/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import PageHome from './Pages/PageHome/PageHome';
 import NoPage from './Pages/NoPage/NoPage';
@@ -11,18 +11,18 @@ import INFtrojka from './Pages/PageEgzaminy/MiniPagesEgzamin/INF03/INFtrojka';
 function App() {
   return ( 
     <div className="App" >
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
-            <Route path="/EgzaminyZawodoweINF" element={<Navbar />}>
+            <Route path="https://PumaYT22.github.io/EgzaminyZawodoweINF" element={<Navbar />}>
               <Route index element={<PageHome />} />
-              <Route path="/EgzaminyZawodoweINF/contact" element={<PageContact />} />
-              <Route path="/EgzaminyZawodoweINF/egzaminy" element={<PageEgzaminy />} />
-                  <Route path="/EgzaminyZawodoweINF/egzaminy/inf03" element={<INFtrojka />} />
+              <Route path="https://PumaYT22.github.io/EgzaminyZawodoweINF/contact" element={<PageContact />} />
+              <Route path="https://PumaYT22.github.io/EgzaminyZawodoweINF/egzaminy" element={<PageEgzaminy />} />
+                  <Route path="https://PumaYT22.github.io/EgzaminyZawodoweINF/egzaminy/inf03" element={<INFtrojka />} />
               
               <Route path="*" element={<NoPage />} />
             </Route>
           </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
