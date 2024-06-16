a32ea2d84d32f1e645744697150f80037f2de5a4
PumaYT22 <pumaxyt@gmail.com>
2024-06-16 19:02:50 +0200

    Update INFtrojka.jsx

--- Strona/egzaminy/src/Pages/PageEgzaminy/MiniPagesEgzamin/INF03/INFtrojka.jsx
+++ Strona/egzaminy/src/Pages/PageEgzaminy/MiniPagesEgzamin/INF03/INFtrojka.jsx
@@ -6,66 +6,66 @@ const repoOwner = 'PumaYT22';
 const repoName = 'EgzaminyZawodoweINF';
 
 const INFtrojka = () => {
-    // const [searchTerm, setSearchTerm] = useState('');
-    // const [currentPage, setCurrentPage] = useState(1);
-    // const [examData, setExamData] = useState([]);
-    // const itemsPerPage = 5;
+    const [searchTerm, setSearchTerm] = useState('');
+    const [currentPage, setCurrentPage] = useState(1);
+    const [examData, setExamData] = useState([]);
+    const itemsPerPage = 5;
 
-    // useEffect(() => {
-    //     const fetchAllExamFolders = async () => {
-    //         try {
-    //             const years = ['2021', '2022', '2023', '2024'];
-    //             const allExamFolders = [];
+    useEffect(() => {
+        const fetchAllExamFolders = async () => {
+            try {
+                const years = ['2021', '2022', '2023', '2024'];
+                const allExamFolders = [];
 
-    //             for (const year of years) {
-    //                 const response = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/Egzaminy/INF03/${year}`);
-    //                 const folders = response.data;
+                for (const year of years) {
+                    const response = await axios.get(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/Egzaminy/INF03/${year}`);
+                    const folders = response.data;
 
-    //                 // Przetwarzanie każdego folderu INF03/{rok}
-    //                 for (const folder of folders) {
-    //                     if (folder.type === 'dir') {
-    //                         const folderName = folder.name;
-    //                         const parts = folderName.split('_');
-    //                         const month = parts[2];
-    //                         const sheetNumber = parts[3];
+                    // Przetwarzanie każdego folderu INF03/{rok}
+                    for (const folder of folders) {
+                        if (folder.type === 'dir') {
+                            const folderName = folder.name;
+                            const parts = folderName.split('_');
+                            const month = parts[2];
+                            const sheetNumber = parts[3];
 
-    //                         allExamFolders.push({
-    //                             name: folderName,
-    //                             year,
-    //                             month,
-    //                             sheetNumber,
-    //                         });
-    //                     }
-    //                 }
-    //             }
+                            allExamFolders.push({
+                                name: folderName,
+                                year,
+                                month,
+                                sheetNumber,
+                            });
+                        }
+                    }
+                }
 
-    //             setExamData(allExamFolders);
-    //         } catch (error) {
-    //             console.error('Error fetching exam folders:', error);
-    //         }
-    //     };
+                setExamData(allExamFolders);
+            } catch (error) {
+                console.error('Error fetching exam folders:', error);
+            }
+        };
 
-    //     fetchAllExamFolders();
-    // }, []);
+        fetchAllExamFolders();
+    }, []);
 
-    // // Filtracja wyników na podstawie wyszukiwania
-    // const filteredExams = examData.filter(exam =>
-    //     exam.name.toLowerCase().includes(searchTerm.toLowerCase())
-    // );
+    // Filtracja wyników na podstawie wyszukiwania
+    const filteredExams = examData.filter(exam =>
+        exam.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
 
-    // // Obliczanie indeksów początkowego i końcowego wyniku na aktualnej stronie
-    // const indexOfLastExam = currentPage * itemsPerPage;
-    // const indexOfFirstExam = indexOfLastExam - itemsPerPage;
-    // const currentExams = filteredExams.slice(indexOfFirstExam, indexOfLastExam);
+    // Obliczanie indeksów początkowego i końcowego wyniku na aktualnej stronie
+    const indexOfLastExam = currentPage * itemsPerPage;
+    const indexOfFirstExam = indexOfLastExam - itemsPerPage;
+    const currentExams = filteredExams.slice(indexOfFirstExam, indexOfLastExam);
 
-    // // Obsługa zmiany strony
-    // const handlePageChange = (pageNumber) => {
-    //     setCurrentPage(pageNumber);
-    // };
+    // Obsługa zmiany strony
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    };
 
     return (
         <div className='d-flex justify-content-center text-center' style={{ minHeight: "80vh" }}>
-           adsas {/* <div className="container py-5">
+          <div className="container py-5">
                 <div className="mb-3">
                     <label htmlFor="searchInput" className="form-label">Wyszukaj arkusz INF03</label>
                     <input
@@ -106,7 +106,7 @@ const INFtrojka = () => {
                         </Pagination.Item>
                     ))}
                 </Pagination>
-            </div> */}
+            </div>
         </div>
     );
 }
