477e04f55db74e2629214306ba27027695fd98c4
PumaYT22 <pumaxyt@gmail.com>
2024-06-16 18:21:41 +0200

    zmiany

--- Strona/egzaminy/src/App.js
+++ Strona/egzaminy/src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
-
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import PageHome from './Pages/PageHome/PageHome';
 import NoPage from './Pages/NoPage/NoPage';
@@ -9,20 +8,19 @@ import PageEgzaminy from './Pages/PageEgzaminy/PageEgzaminy';
 import INFtrojka from './Pages/PageEgzaminy/MiniPagesEgzamin/INF03/INFtrojka';
 
 function App() {
-  return ( 
-    <div className="App" >
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Navbar />}>
-              <Route index element={<PageHome />} />
-              <Route path="/contact" element={<PageContact />} />
-              <Route path="/egzaminy" element={<PageEgzaminy />} />
-                  <Route path="/egzaminy/inf03" element={<INFtrojka />} />
-              
-              <Route path="*" element={<NoPage />} />
-            </Route>
-          </Routes>
-      </BrowserRouter>
+  return (
+    <div className="App">
+      <HashRouter>
+        <Routes>
+          <Route path="/EgzaminyZawodoweINF" element={<Navbar />}>
+            <Route index element={<PageHome />} />
+            <Route path="/EgzaminyZawodoweINF/contact" element={<PageContact />} />
+            <Route path="/EgzaminyZawodoweINF/egzaminy" element={<PageEgzaminy />} />
+            <Route path="/EgzaminyZawodoweINF/egzaminy/inf03" element={<INFtrojka />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
